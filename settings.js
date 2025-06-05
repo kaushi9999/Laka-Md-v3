c9be1a28f5b7929d2ec5d906315f4d1f0f519e46
NIMA-XD <wihanganirmal7@gmail.com>
2025-06-05 22:20:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=dhhGzLAZ#p8BChg4wP6knmD68eeeIHlEThPOTZcKS1bzzqPd9RDc",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=lk4DEaDA#gSF2Fp06VLXNgv3C3_GEubDrT7t0eL3akwrQCbwYE7Y",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
